3a9f4ccef9e172c1257ca8ba09b4a23ce4a94bea
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-11 16:20:32 +0900

    tweet feed, create but incomplete

--- tweet/urls.py
+++ tweet/urls.py
@@ -6,9 +6,5 @@ from tweet import views
 urlpatterns = [
     path('feed', views.index),
     path('feed/create', views.todo_create),
-    # path('tweet/<int:tweet_id>/', views.todo_read),
-    # path('<int:twwet_id>/update/', views.todo_update),
-    # path('<int:tweet_id>/delete/', views.todo_delete),
-
     
 ]
