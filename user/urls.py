41e7f82c20b7088648bdff50c039b63a0f5fa329
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-11 11:31:55 +0900

    add signup view, changing name: show_poops->home

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name="index"),
-    path('show_poops/', views.show_poops,name="show_poops"),
+    path('home/', views.show_home,name="home"),
 ]
--- user/views.py
+++ user/views.py
@@ -6,7 +6,8 @@ def index(request):
     return render(request,'user/index.html')
 
 
-def show_poops(request):
-    return render(request,'user/show_poops.html')
+def show_home(request):
+    return render(request,'user/home.html')
 
-
+def signup(request):
+    return render(request, 'account/signup.html')
